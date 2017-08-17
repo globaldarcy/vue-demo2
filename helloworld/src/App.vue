1df593091a68d27f4e950e3d2d3da226ca8e38ef
Shawn <289904316@qq.com>
2017-08-17 17:11:47 +0800

    test

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
@@ -7,20 +7,49 @@
 
 <script>
 import Hello from './components/Hello'
-import hi from './components/hi'
 export default {
   name: 'app',
-  components: {Hello, hi}
+  components: {Hello}
 }
-</script>
+</script>-->
+
+
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>导航</h1>
+    <p>{{ $route.name }}</p>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+    <router-view></router-view>
+    <router-view name="left" class="left"></router-view>
+    <router-view name="right" class="right"></router-view>
+  </div>
+</template>
+
+
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.left{
+  float: left;
+  width:50%;
+  background: greenyellow;
+  height: 300px;
+}
+.right{
+  float: left;
+  width:50%;
+  background: silver;
+  height: 300px;
 }
 </style>
